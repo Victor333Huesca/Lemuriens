614784360e4a4c7d9d9c98a8c6b06c39d2459844
Victor333Huesca <victor333huesca@hotmail.com>
2017-10-22 18:36:27 +0200

    Ajout du site
    Ajout du site web et préparation de l'arborescence du projet.

---
+++ Site_Web/public_html/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta charset="utf-8" />
+	<title>Lemuriens</title>
+	<link rel="icon" type="image/png" href="lemuriens.png" />
+	<link rel="stylesheet" type="text/css" href="index.css" />
+</head>
+<body>
+	<h1>Bienvenue sur le site des lémuriens !</h1>
+	<p>Les lémuriens sont actuellement en cours de préparation du site. Par conséquent nous vous prions de bien vouloir attendre quelques temps afin de leur laisser le temps de contruire le site (car c'est lourd les briques pour ces pauvres pitites bêtes).'</p>
+</body>
+
+</html>
